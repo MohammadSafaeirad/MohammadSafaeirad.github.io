ff3cd11ec6f3d6507a769833805304acdcc3b412
mohammadsr1 <m.safaei81@hotmail.com>
2023-09-25 18:25:33 -0400

    Logo and skills description updated

--- form.php
+++ form.php
@@ -1,23 +1,32 @@
+
+
 <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST['username'], $_POST['email'], $_POST['subject'], $_POST['contact_message'])) {
+        $name = $_POST['username'];
+        $visitor_email = $_POST['email'];
+        $subject = $_POST['subject'];
+        $message = $_POST['contact_message'];
 
+        $email_from = "mohammad.ml";
+        $email_subject = "New form submission.";
+        $email_body = "User name: $name.\n" .
+            "User email: $visitor_email.\n" .
+            "User subject: $subject.\n" .
+            "User message: $message.\n";
 
-$name = $_POST['username'];
-$vicitor_email = $_POST['email'];
-$subject = $_POST['subject'];
-$message = $_POST['contact_message'];
+        $to = "m.safaei81@hotmail.com";
 
-$email_from = "kumarvicku.ml";
-$email_subject = "new form submisssion.";
-$email_body = "user name: $name.\n".
-            "user email: $visitor_email.\n".
-            "user subject: $subject.\n".
-            "user message: $message.\n";
+        $headers = "From: $email_from \r\n";
+        $headers .= "Reply-To: $visitor_email \r\n";
 
-            $to = "kvicku44@gmail.com";
-
-            $headers = "from: $email_from \r\n";
-            $headers .= "reply-to: $visitor_email \r\n";
-
-            mail($to,$email_subject,$email_body,$headers);
-            header("location: index.html");
+        if (mail($to, $email_subject, $email_body, $headers)) {
+            header("Location: index.html");
+            exit; // Stop script execution after redirection
+        } else {
+            echo "Failed to send email. Please try again later.";
+        }
+    }
+}
 ?>
+
